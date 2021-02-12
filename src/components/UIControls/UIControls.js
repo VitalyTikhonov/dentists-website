f17157cbb929fedd432cd8dd15b50943eb28ed02
Vitaliy Tikhonov <vitaliy.a.tikhonov@yandex.ru>
2021-02-12 11:45:59 +0400

    019 Implemented SCSS inheritance

--- src/components/UIControls/UIControls.js
+++ src/components/UIControls/UIControls.js
@@ -1,42 +1,60 @@
-import styles from "./UIControls.module.scss";
-import getCN from "classnames";
+import styles from "./UIDevContainer.module.scss";
+import cn from "classnames";
 
 const UIControls = function (props) {
   return (
-    <div className={getCN(styles.UIDevContainer)}>
+    <div className={cn(styles.UIDevContainer)}>
       <div
-        className={getCN(styles.UIDevContainer__pageWidthSection, styles.UIDevContainer__sectionWithPhotoBg)}
+        className={cn(
+          styles.UIDevContainer__pageWidthSection,
+          styles.UIDevContainer__sectionWithPhotoBg
+        )}
       >
-        <div className={getCN(styles.UIDevContainer__controlsOnTheRight)} >
-          <div className={getCN(styles.cover__topControlOverlay)} >
-            <button className={getCN(styles.btn__inCover, styles.btn__withIcon, styles.btn__inCover_principal)}>
-              <span className={getCN(styles.btn__iconInBtn, styles.btn__iconInBtn_accountIcon)} />
+        <div className={cn(styles.UIDevContainer__controlsOnTheRight)}>
+          <div className={cn(styles.cover__topControlOverlay)}>
+            <button className={cn(styles.button, styles.inCover, styles.inCover_principal)}>
+              <span className={cn(styles.iconInBtn_accountIcon)} />
               Личный кабинет
             </button>
-            <button className={getCN(styles.btn__inCover, styles.btn__withIcon, styles.btn__inCover_secondary)}>
-              <span className={getCN(styles.btn__iconInBtn, styles.btn__iconInBtn_logoutIcon)} />
+            <button className={cn(styles.button, styles.inCover, styles.inCover_secondary)}>
+              <span className={cn(styles.iconInBtn_logoutIcon)} />
               Выйти
             </button>
           </div>
 
-          <div className={getCN(styles.cover__topControlOverlay)} >
-            <button className={getCN(styles.btn__inCover, styles.btn__withIcon, styles.btn__inCover_principal)}>
-              <span className={getCN(styles.btn__iconInBtn, styles.btn__iconInBtn_loginIcon)} />
+          <div className={cn(styles.cover__topControlOverlay)}>
+            <button className={cn(styles.button, styles.inCover, styles.inCover_principal)}>
+              <span className={cn(styles.iconInBtn_loginIcon)} />
               Войти
             </button>
-            <button className={getCN(styles.btn__inCover, styles.btn__withIcon, styles.btn__inCover_secondary)}>
-              <span className={getCN(styles.btn__iconInBtn, styles.btn__iconInBtn_signupIcon)} />
+            <button className={cn(styles.button, styles.inCover, styles.inCover_secondary)}>
+              <span className={cn(styles.iconInBtn_signupIcon)} />
               Регистрация
             </button>
           </div>
         </div>
       </div>
-      <div className={getCN(styles.UIDevContainer__columnContainer)}>
-        <div className={getCN(styles.UIDevContainer__column, styles.UIDevContainer__column_bgBlue)}></div>
-        <div className={getCN(styles.UIDevContainer__column, styles.UIDevContainer__column_bgLightBlue)}></div>
-        <div className={getCN(styles.UIDevContainer__column, styles.UIDevContainer__column_bgWallpaper)}></div>
+      <div className={cn(styles.UIDevContainer__columnContainer)}>
+        <div
+          className={cn(
+            styles.UIDevContainer__column,
+            styles.UIDevContainer__column_bgBlue
+          )}
+        ></div>
+        <div
+          className={cn(
+            styles.UIDevContainer__column,
+            styles.UIDevContainer__column_bgLightBlue
+          )}
+        ></div>
+        <div
+          className={cn(
+            styles.UIDevContainer__column,
+            styles.UIDevContainer__column_bgWallpaper
+          )}
+        ></div>
       </div>
-      {/* <div className={getCN(styles.)} ></div> */}
+      {/* <div className={cn(styles.)} ></div> */}
     </div>
   );
 };
