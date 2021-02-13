92d275b4651436654dd41209a0c5b18fcbbfbfdb
Vitaliy Tikhonov <vitaliy.a.tikhonov@yandex.ru>
2021-02-13 22:35:17 +0400

    023 Made button panel

---
+++ src/images/UI-control-elements/svg/SvgComponents/DoubleChevron.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+function SvgDoubleChevron(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 407.436 407.436"
+      {...props}
+    >
+      <path d="M162.231 0l-21.248 21.178 181.946 182.54-181.946 182.54 21.248 21.178 203.055-203.718z" />
+      <path d="M63.397 0L42.149 21.178l181.946 182.54-181.946 182.54 21.248 21.178 203.055-203.718z" />
+    </svg>
+  );
+}
+
+export default SvgDoubleChevron;
---
+++ src/images/UI-control-elements/svg/SvgComponents/Menu.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+function SvgMenu(props) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 384" {...props}>
+      <path d="M0 277.333h384V320H0zM0 170.667h384v42.667H0zM0 64h384v42.667H0z" />
+    </svg>
+  );
+}
+
+export default SvgMenu;
