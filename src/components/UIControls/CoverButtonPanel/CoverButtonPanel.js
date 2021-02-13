1e21c759a5fe3ac25633998d5bba1329ed95c7e4
Vitaliy Tikhonov <vitaliy.a.tikhonov@yandex.ru>
2021-02-13 22:38:22 +0400

    024 Made button panel

--- src/components/UIControls/CoverButtonPanel/CoverButtonPanel.js
+++ src/components/UIControls/CoverButtonPanel/CoverButtonPanel.js
@@ -1,6 +1,11 @@
 import { memo } from "react";
 import { HashRouter, Link } from "react-router-dom";
 import "./CoverButtonPanel.scss";
+import ServicesIcon from "../../../images/UI-control-elements/svg/SvgComponents/ServicesIcon.js";
+import AppointmentIcon from "../../../images/UI-control-elements/svg/SvgComponents/AppointmentIcon.js";
+import FemaleDentistIcon from "../../../images/UI-control-elements/svg/SvgComponents/FemaleDentistIcon.js";
+import FeatherPen from "../../../images/UI-control-elements/svg/SvgComponents/IconFeatherPen.js";
+import Doctors from "../../../images/UI-control-elements/svg/SvgComponents/Doctors.js";
 
 const CoverButtonPanel = memo(function (props) {
   return (
@@ -15,27 +20,31 @@ const CoverButtonPanel = memo(function (props) {
           hashType="noslash"
         >
           <li className="cover__menu-major-item button list-item-unstyling">
-            <Link to="services" className="cover__menu-major-link button__ic-lab_type_services link-unstyling">
+            <Link to="services" className="cover__menu-major-link link-unstyling">
+              <ServicesIcon className="cover__menu-icon" />
               Услуги
             </Link>
           </li>
           <li className="cover__menu-major-item button list-item-unstyling">
             <Link
               to="book_appointment"
-              className="cover__menu-major-link button__ic-lab_type_book_appointment link-unstyling"
+              className="cover__menu-major-link link-unstyling"
             >
+              <AppointmentIcon className="cover__menu-icon" />
               Запись на приём
             </Link>
           </li>
           <li className="cover__menu-major-item button list-item-unstyling">
-            <Link to="about_doctor" className="cover__menu-major-link button__ic-lab_type_about_doctor link-unstyling">
+            <Link to="about_doctor" className="cover__menu-major-link link-unstyling">
+              <FemaleDentistIcon className="cover__menu-icon cover__menu-icon_larger" />
               Обо мне
             </Link>
           </li>
         </HashRouter>
         {true && (
           <li className="cover__menu-major-item button list-item-unstyling">
-            <Link to="blog" className="cover__menu-major-link button__ic-lab_type_blog link-unstyling">
+            <Link to="blog" className="cover__menu-major-link link-unstyling">
+              <FeatherPen className="cover__menu-icon" />
               Мой блог
             </Link>
           </li>
@@ -44,8 +53,9 @@ const CoverButtonPanel = memo(function (props) {
           <li className="cover__menu-major-item button list-item-unstyling">
             <Link
               to="blog/for_colleagues"
-              className="cover__menu-major-link button__ic-lab_type_for_colleagues link-unstyling"
+              className="cover__menu-major-link link-unstyling"
             >
+              <Doctors className="cover__menu-icon" />
               Коллегам
             </Link>
           </li>
