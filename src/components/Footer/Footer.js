ecb69eb7c1647b087473401fc97a1531c6345cb4
Vitaliy Tikhonov <vitaliy.a.tikhonov@yandex.ru>
2021-10-22 23:15:07 +0300

    Made and styled the Flaticon link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,7 +10,11 @@ function Footer() {
       <div className="footer__info" >
         <p className="footer__info-line" >© Наталья Натфуллина, 2021, содержание, дизайн</p>
         <p className="footer__info-line" >© Виталий Тихонов, 2021, исходный код, дизайн</p>
-        <p className="footer__info-line" >Авторы иконок: Icongeek26, monkik, prettycons, Kiranshastry, Pixel perfect, Vitaly Gorbachev, Those Icons, Kiranshastry, Pixel perfect, Lyolya, Freepik, источник: www.flaticon.com</p>
+        <p className="footer__info-line" >
+          Авторы иконок: Icongeek26, monkik, prettycons, Kiranshastry,
+          Pixel perfect, Vitaly Gorbachev, Those Icons, Lyolya, Freepik.
+          Источник: <a className="footer__link" href="https:\\www.flaticon.com" target="_blank" rel="noopener noreferrer">www.flaticon.com</a>
+        </p>
       </div>
     </footer>
   )
