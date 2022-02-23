3b5adb3bf1b20f9495d2b82971556d4be3565ceb
Vitaliy Tikhonov <vitaliy.a.tikhonov@yandex.ru>
2022-02-23 06:57:31 +0300

    Revert "GH-Pages Deployment settings reversed"
    This reverts commit cd42b19bc366a11b6d2f31f0ee62f7d79258770a.

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter >
+      <BrowserRouter basename="/DrNatfullina" >
         <App />
       </BrowserRouter>
     </Provider>
